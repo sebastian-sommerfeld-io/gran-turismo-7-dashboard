1516248ce1039b9c997c8035711f2eab671f0c78
sebastian <sebastian@sommerfeld.io>
2022-10-13 23:20:21 +0200

    feat: initialized angular project

--- src/main/angular-cli/angular-cli.sh
+++ src/main/angular-cli/angular-cli.sh
@@ -9,8 +9,11 @@
 # The script does not accept any parameters.
 
 
-CMD_BUILD="build_image"
-CMD_INIT="init_webapp"
+CMD_CLI_BUILD="build_image"
+CMD_NG_INIT="init"
+CMD_NG_SERVE="serve_dev"
+CMD_NG_BUILD="build"
+CMD_NG_TEST="unit_test"
 
 ANTORA_CLI="local/ansible-cli:dev"
 
@@ -26,7 +29,7 @@ function initWebapp() {
 
     if [ -d "$WEBAPP_DIR" ]; then
       echo -e "$LOG_ERROR Cannot initialize Angular Webapp. Directory '$P$WEBAPP_DIR$D' already exists."
-      echo -e "$LOG_ERROR Exit" && exit
+      echo -e "$LOG_ERROR Exit" && exit 8
     fi
 
     docker run --rm \
@@ -36,11 +39,33 @@ function initWebapp() {
   )
 }
 
+# @description Serve the Angular webapp on http://localhost:4200.
+#
+# @arg $1 string ``ng`` command - mandatory
+# @exitcode 8 If param missing
+function ng() {
+  if [ -z "$1" ]
+  then
+    echo -e "$LOG_ERROR Param missing"
+    echo -e "$LOG_ERROR exit" && exit 8
+  fi
+
+  (
+    cd "../$WEBAPP_DIR" || exit
+
+    docker run --rm \
+      --volume "$(pwd):$(pwd)" \
+      --workdir "$(pwd)" \
+      --network host \
+      "$ANTORA_CLI" ng "$1"
+  )
+}
+
 
 echo -e "$LOG_INFO What should I do?"
-select o in "$CMD_BUILD" "$CMD_INIT"; do
+select o in "$CMD_CLI_BUILD" "$CMD_NG_INIT" "$CMD_NG_SERVE" "$CMD_NG_TEST" "$CMD_NG_BUILD"; do
   case "$o" in
-    "$CMD_BUILD" )
+    "$CMD_CLI_BUILD" )
       echo -e "$LOG_INFO Lint dockerfile"
       docker run --rm -i hadolint/hadolint:latest < Dockerfile
 
@@ -48,9 +73,26 @@ select o in "$CMD_BUILD" "$CMD_INIT"; do
       docker build -t "$ANTORA_CLI" .
 
       break;;
-    "$CMD_INIT" )
+    "$CMD_NG_INIT" )
       initWebapp
-      
+      break;;
+    "$CMD_NG_SERVE" )
+      echo -e "$LOG_INFO Serve Angular webapp on http://localhost:4200"
+      ng serve
+      break;;
+    "$CMD_NG_TEST" )
+      echo -e "$LOG_INFO Execute the unit tests via Karma"
+      ng test
+      break;;
+    "$CMD_NG_BUILD" )
+      echo -e "$LOG_INFO Build Angular webapp"
+      ng build
+
+      (
+        echo -e "$LOG_INFO Serve finished Angular build on http://localhost:4201"
+        cd "../$WEBAPP_DIR" || exit
+        docker run -it --rm -p 4201:80 -v "$(pwd)/dist/webapp":/usr/local/apache2/htdocs/ httpd:2.4
+      )
       break;;
   esac
 done
